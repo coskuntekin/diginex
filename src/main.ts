a3f9b8f3a7530e1ccaabbd37b26695bf749442ac
coskuntekin <mail@coskuntekin.com>
2025-06-07 20:33:21 +0300

    refactor: restructure main.ts for improved app initialization and state management

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
+import "@/assets/stylesheets/tailwind.css";
+import "element-plus/es/components/notification/style/css";
+import { createPinia } from "pinia";
 import { createApp } from "vue";
-import "./style.css";
 import App from "./App.vue";
+import router from "./router";
 
-createApp(App).mount("#diginex");
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(pinia);
+app.use(router);
+
+app.mount("#diginex");
