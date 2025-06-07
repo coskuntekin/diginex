3a127fc3e6492b2e51bd337bc3e90b93b77e5e79
coskuntekin <mail@coskuntekin.com>
2025-06-07 20:33:13 +0300

    feat: enhance Vite configuration with environment loading and component auto-importing

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,43 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-});
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return defineConfig({
+    plugins: [
+      vue(),
+      tailwindcss(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+        imports: ['vue'],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    server: {
+      host: true,
+      cors: true,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+          ws: false,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  });
+};
