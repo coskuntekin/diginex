e87421d7c287462e108683548e466e4e06953cc9
coskuntekin <mail@coskuntekin.com>
2025-06-07 20:35:40 +0300

    feat: create index file to export service modules

---
+++ src/services/index.ts
@@ -0,0 +1,7 @@
+export { default as api } from './api';
+export { BaseService } from './baseService';
+export { AuthService, authService } from './authService';
+export { UserService, userService } from './userService';
+export { NotificationService, notificationService } from './notificationService';
+
+export * from '../types/api';
