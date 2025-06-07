a87ff16490387b2cae47b1f53c6715d4cf839d77
coskuntekin <mail@coskuntekin.com>
2025-06-07 23:02:13 +0300

    feat: add missing Element Plus styles for improved UI components

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import "@/assets/stylesheets/tailwind.css";
+import "@/assets/stylesheets/element.css";
 import "element-plus/es/components/notification/style/css";
+import "element-plus/es/components/message-box/style/css";
+import "element-plus/es/components/pagination/style/css";
+import "element-plus/es/components/dialog/style/css";
 import { createPinia } from "pinia";
 import { createApp } from "vue";
 import App from "./App.vue";
